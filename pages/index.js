acc97ebe8e840e68c95ddac73a48ea59fd73e997
Wu Wei <vc3104@gmail.com>
2022-05-24 09:10:57 +0900

    add font

--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,24 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Navbar />
+    <>
+      <Head>
+        <title>Home App</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <div className={styles.container}>
+        <Navbar />
 
       <main className={styles.main}>
       </main>
 
-      <footer className={styles.footer}>
-       
-      </footer>
-    </div>
+        </footer>
+      </div>
+    </>
   )
 }

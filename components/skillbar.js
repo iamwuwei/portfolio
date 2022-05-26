be34f9819a0e328933bd5840c9cc84622099f39d
Wu Wei <vc3104@gmail.com>
2022-05-27 07:37:26 +0900

    create skillbar component

---
+++ components/skillbar.js
@@ -0,0 +1,16 @@
+import styles from '../styles/skillbar.module.scss'
+const SkillBar = ({title, percentage}) => {
+    return(
+        <div className={styles.skillbar}>
+            <div className={styles.title}>
+                <h1>{title}</h1>
+                <h1>{percentage}%</h1>
+            </div>
+            <div className={styles.bar}>
+               <div style={{backgroundColor: 'blue', width: `${percentage}%`}}></div>
+            </div>
+        </div>
+    )
+}
+
+export default SkillBar

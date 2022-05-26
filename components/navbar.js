2833c6d9e9011b3e8b74cc95b341274cd8d65341
Wu Wei <vc3104@gmail.com>
2022-05-27 07:40:15 +0900

    create multilang dropbox

--- components/navbar.js
+++ components/navbar.js
@@ -1,6 +1,6 @@
 import styles from '../styles/Navbar.module.css'
 import Image from 'next/image'
-import darkModeMoonSvg from '../public/dark-mode-moon.svg'
+import earthSvg from '../public/earth-globe.svg'
 import snorlax from '../public/snorlax.png'
 
 const Navbar = () => {
@@ -9,17 +9,32 @@ const Navbar = () => {
             <div className={styles['navbar-container']}>
                 <div className={styles['navbar-element-wrapper']}>
                     <div className={styles.name}>
+                        <div className={styles.snorlax}>
+                            <Image src={snorlax} ></Image>
+                        </div>
                         <h1>
                             Wu Wei
                         </h1>
                     </div>
-                    <div className={styles['dark-mode-button-wrapper']}>
-                        <label className={styles.switch}>
-                            <input type="checkbox"/>
-                            <span className={`${styles.slider} ${styles.round}`}></span>
-                            <span className={styles.sun}></span>
-                            <span className={styles.moon}></span>
-                        </label>
+                    <div className={styles['button-container']}>
+                        <div className={`${styles['button-wrapper']} ${styles.earth}`}>
+                            <div className={styles.dropdown}>
+                                <Image src={earthSvg} className={styles.dropbtn}></Image>
+                                <div className={styles['dropdown-content']}>
+                                    <a href="#">日本語</a>
+                                    <a href="#">English</a>
+                                    <a href="#">繁體中文</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles['button-wrapper']}>
+                            <label className={styles.switch}>
+                                <input type="checkbox" />
+                                <span className={`${styles.slider} ${styles.round}`}></span>
+                                <span className={styles.sun}></span>
+                                <span className={styles.moon}></span>
+                            </label>
+                        </div>
                     </div>
                 </div>
             </div>

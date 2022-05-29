c6772e5d9109107a41332945e10bff5e19800401
Wu Wei <vc3104@gmail.com>
2022-05-30 01:08:22 +0900

    add animation to skillbar

--- components/skillbar.js
+++ components/skillbar.js
@@ -1,13 +1,14 @@
 import styles from '../styles/skillbar.module.scss'
-const SkillBar = ({title, percentage}) => {
-    return(
+const SkillBar = ({ title, percentage }) => {
+
+    return (
         <div className={styles.skillbar}>
             <div className={styles.title}>
                 <h1>{title}</h1>
                 <h1>{percentage}%</h1>
             </div>
             <div className={styles.bar}>
-               <div style={{backgroundColor: 'blue', width: `${percentage}%`}}></div>
+                <div style={{ backgroundColor: 'blue', width: `${percentage}%`}} className="animation"></div>
             </div>
         </div>
     )

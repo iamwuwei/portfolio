0ebbfad410874dc79dcb15d7622f3e2b2e6680bf
Wu Wei <vc3104@gmail.com>
2022-06-04 07:07:33 +0900

    add darkmode

--- components/navbar.js
+++ components/navbar.js
@@ -1,10 +1,40 @@
 import Link from 'next/link'
+import {useState, useEffect} from 'react'
 import styles from '../styles/Navbar.module.css'
 import Image from 'next/image'
 import earthSvg from '../public/earth-globe.svg'
 import snorlax from '../public/snorlax.png'
 
 const Navbar = () => {
+    const [darkTheme, setDarkTheme] = useState(undefined);
+
+  const handleToggle = (event) => {
+    setDarkTheme(event.target.checked);
+  };
+
+  useEffect(() => {
+    if (darkTheme !== undefined) {
+      if (darkTheme) {
+        // Set value of  darkmode to dark
+        document.documentElement.setAttribute('data-theme', 'dark');
+        window.localStorage.setItem('theme', 'dark');
+      } else {
+        // Set value of  darkmode to light
+        document.documentElement.removeAttribute('data-theme');
+        window.localStorage.setItem('theme', 'light');
+      }
+    }
+  }, [darkTheme]);
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    const initialColorValue = root.style.getPropertyValue(
+      '--initial-color-mode'
+    );
+    // Set initial darkmode to light
+    setDarkTheme(initialColorValue === 'dark');
+  }, []);
+
     return (
         <nav className={styles.navbar}>
             <div className={styles['navbar-container']}>
@@ -36,7 +66,7 @@ const Navbar = () => {
                         </div>
                         <div className={styles['button-wrapper']}>
                             <label className={styles.switch}>
-                                <input type="checkbox" />
+                                <input type="checkbox" checked={darkTheme} onChange={handleToggle}/>
                                 <span className={`${styles.slider} ${styles.round}`}></span>
                                 <span className={styles.sun}></span>
                                 <span className={styles.moon}></span>
--- pages/_document.js
+++ pages/_document.js
@@ -6,16 +6,57 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link
           href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600&display=swap"
           rel="stylesheet"
         />
       </Head>
       <body>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: themeInitializerScript,
+          }}
+        ></script>
         <Main />
         <NextScript />
       </body>
     </Html>
   )
+}
+// This function needs to be a String
+const themeInitializerScript = `(function() {
+	${setInitialColorMode.toString()}
+	setInitialColorMode();
+})()
+`;
+
+function setInitialColorMode() {
+  // Check initial color preference
+  function getInitialColorMode() {
+    const persistedPreferenceMode = window.localStorage.getItem('theme');
+    const hasPersistedPreference = typeof persistedPreferenceMode === 'string';
+
+    if (hasPersistedPreference) {
+      return persistedPreferenceMode;
+    }
+
+    // Check the current preference
+    const preference = window.matchMedia('(prefers-color-scheme: dark)');
+    const hasMediaQueryPreference = typeof preference.matches === 'boolean';
+
+    if (hasMediaQueryPreference) {
+      return preference.matches ? 'dark' : 'light';
+    }
+
+    return 'light';
+  }
+
+  const currentColorMode = getInitialColorMode();
+  const element = document.documentElement;
+  element.style.setProperty('--initial-color-mode', currentColorMode);
+
+  // If darkmode apply darkmode
+  if (currentColorMode === 'dark')
+    document.documentElement.setAttribute('data-theme', 'dark');
 }

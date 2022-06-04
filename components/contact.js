56f5f8d07ff947c64bfad4544c20c68b035e392c
Wu Wei <vc3104@gmail.com>
2022-06-04 09:23:43 +0900

    挙動の修正

--- components/contact.js
+++ components/contact.js
@@ -6,11 +6,16 @@ const Contact = ({data}) => {
     const contactContainerRef = useRef()
     const [isMessageSection, setMessageSection] = useState(false)
     const [isContactSection, setContactSection] = useState(false)
+    const [isInputing, setInputing] = useState(false)
+    const [isSentSuccessed, setSentSuccessed] = useState(false)
+    const [isSentFailed, setSentFailed] = useState(false)
     const [form, setForm] = useState({ "name": "", "email": "", "message": "" })
     const [loading, setLoading] = useState(false)
     const [alertMsg, setAlertMsg] = useState("")
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setSentSuccessed(false)
+        setSentFailed(false)
         setLoading(true);
         if (!validateInput()) {
             await setTimeout(() => {
@@ -30,7 +35,10 @@ const Contact = ({data}) => {
             }).then((result) => {
                 if (result.status != 200) {
                     result.json().then(data => { console.log(data.message) })
+                    setSentFailed(true)
                 }
+                else
+                    setSentSuccessed(true)
 
             }).catch((err) => {
                 console.log(err)
@@ -48,7 +56,10 @@ const Contact = ({data}) => {
     const handleFormChange = (e) => {
         setForm({ ...form, [e.target.name]: e.target.value })
     }
-
+    const hadleInputing = (e) =>{
+        if(!isInputing)
+            setInputing(true)
+    }
     const validateEmail = (emailId) => {
         var mailformat = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
         if (emailId.match(mailformat)) {
@@ -86,18 +97,25 @@ const Contact = ({data}) => {
         }
     }, [])
 
+    useEffect(() => {
+       if(!isMessageSection){
+           setInputing(false)
+           setSentFailed(false)
+           setSentSuccessed(false)
+       }
+    }, [isMessageSection])
     return (
         <div className={styles.contactContainer} ref={contactContainerRef}>
             <form onSubmit={handleSubmit}>
                 <div className={styles.contact} >
                     <h3>Your Name</h3>
-                    <input type="text" placeholder="Name" name="name" value={form.name} onChange={handleFormChange} >
+                    <input type="text" placeholder="Name" name="name" value={form.name} onChange={handleFormChange} onClick={hadleInputing}>
                     </input>
                     <h3>Email</h3>
-                    <input type="text" placeholder='Email' name="email" value={form.email} onChange={handleFormChange}>
+                    <input type="text" placeholder='Email' name="email" value={form.email} onChange={handleFormChange} onClick={hadleInputing}>
                     </input>
                     <h3>Message</h3>
-                    <textarea placeholder='Message' name="message" value={form.message} onChange={handleFormChange} ref={messageRef}>
+                    <textarea placeholder='Message' name="message" value={form.message} onChange={handleFormChange} ref={messageRef} onClick={hadleInputing}>
                     </textarea>
                     <div className={styles.submitContainer}>
                         <input type="submit" disabled={loading} className={`${styles.submitBtn} ${loading ? styles.loading : ''}`} value="Send Message"></input>
@@ -109,13 +127,21 @@ const Contact = ({data}) => {
                 </div>
             </form>
 
-            <div className={styles.hello} style={{ display: `${isContactSection ? 'flex' : 'none'}` }}>
-                <div className={`${styles.message} ${styles.messageReceiver}`} style={{ display: `${isContactSection ? 'flex' : 'none'}` }}>
+            <div className={styles.hello} style={{ display: `${isContactSection? 'flex' : 'none'}` }}>
+                <div className={`${styles.message} ${styles.messageReceiver}`} style={{ display: `${(isContactSection&& !isInputing) ? 'flex' : 'none'}` }}>
                     <div className={styles.messageBody}>{data.wellcomeMsg}</div>
                 </div>
 
-                <div className={`${styles.message} ${styles.messageReceiver}`} style={{ display: `${isMessageSection ? 'flex' : 'none'}` }}>
+                <div className={`${styles.message} ${styles.messageReceiver}`} style={{ display: `${(isMessageSection && !isInputing) ? 'flex' : 'none'}` }}>
                     <div className={styles.messageBody}>{data.msg}</div>
+                </div>
+
+                <div className={`${styles.message} ${styles.messageReceiver}`} style={{ display: `${(isSentSuccessed) ? 'flex' : 'none'}` }}>
+                    <div className={styles.messageBody}>{data.msgSuccessed}</div>
+                </div>
+
+                <div className={`${styles.message} ${styles.messageReceiver}`} style={{ display: `${(isSentFailed) ? 'flex' : 'none'}` }}>
+                    <div className={styles.messageBody}>{data.msgError}</div>
                 </div>
             </div>
         </div>
